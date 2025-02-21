924b799b216f4f35327244597aca5cab272cc3ad
Deepak Verma <deepakradialcode@gmail.com>
2025-02-21 09:25:51 +0530

    updates faq

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -2,38 +2,47 @@ import { FAQ_LIST } from "@/utils/helper";
 import React, { useState } from "react";
 import Heading from "./common/Heading";
 import Description from "./common/Description";
+import { FaqArrow } from "@/utils/icons";
 
 const Faq = () => {
-  const [active, setActive] = useState();
+ const [openIndex, setOpenIndex] = useState(0);
 
-  const handleClick = (i) => {
-    setActive(active === i ? "" : i);
-  };
+ const toggleAccordion = (index: any) => {
+   setOpenIndex(openIndex === index ? false : index);
+ };
   return (
     <div className="pt-32 pb-[146px] max-md:py-20 max-sm:py-12 bg-lightGray">
       <div className="max-w-[1172px] mx-auto px-4">
-        <Heading text="FAQs" />
-        <div className="mx-auto flex items-center flex-col pt-[9px]">
-          {FAQ_LIST.map((obj, i) => (
-            <div
-              key={i}
-              onClick={() => handleClick(i)}
-              className="border-b border-gray"
+        <Heading text="FAQs" myClass="pb-[9px]" />
+        {FAQ_LIST.map((data, index: number) => (
+          <div
+            key={index}
+            className="overflow-hidden min-h-[45px] border-b border-gray"
+          >
+            <button
+              onClick={() => toggleAccordion(index)}
+              className="text-[21px] font-semibold leading-[21px] max-lg:text-xl max-md:text-lg text-start max-sm:text-base max-lg:leading-[18px] max-md:leading-[16px] max-sm:leading-[14px] flex justify-between items-center w-full py-2"
             >
-              <div className="cursor-pointer flex items-center justify-between">
-                <h2 className="font-semibold text-[21px] leading-[21px] pt-2">
-                  {obj.title}
-                </h2>
-              </div>
-              <Description
-                text={obj.description}
-                myClass={`transition-all duration-300 ease-linear pb-[14px] ${
-                  active === i ? "h-[70px]" : "h-0 opacity-0"
+              {data.title}
+              <span
+                className={`ml-2 ${
+                  openIndex === index
+                    ? "rotate-90 transition-all duration-300"
+                    : "rotate-0 transition-all duration-300"
                 }`}
-              />
+              >
+                <FaqArrow />
+              </span>
+            </button>
+            <div
+              className={`max-md:overflow-y-auto ${
+                openIndex === index ? "max-h-20 pb-3.5" : "max-h-0"
+              } transition-all duration-500 overflow-hidden`}
+            >
+              <Description text={data.description} />
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
